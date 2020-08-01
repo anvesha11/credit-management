d3b82636dee35a322eeae80a114cd6df501047dc
Anvesha Shandilya <anvesha.shandilya@gmail.com>
2020-08-01 19:16:24 +0530

    Heroku settings

--- credit/settings.py
+++ credit/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = '!u0&kqosb8jg^71@hf16$ja=^x+rga-g7)$v9%77-(z5jr@smy'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
